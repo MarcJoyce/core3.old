6e49c152ea455aeb05ea4d4d7c5bddc9912b1a4b
Cyvster <41338761+Cyvster@users.noreply.github.com>
2024-03-25 17:26:53 -0500

    Update DraftSchematicObjectTemplate.cpp
    crafting - set factory crate size max to 1000: DraftSchematicObjectTemplate.cpp

--- MMOCoreORB/src/templates/intangible/DraftSchematicObjectTemplate.cpp
+++ MMOCoreORB/src/templates/intangible/DraftSchematicObjectTemplate.cpp
@@ -39,7 +39,7 @@ DraftSchematicObjectTemplate::DraftSchematicObjectTemplate() {
 	skillMods.setNoDuplicateInsertPlan();
 	skillMods.setNullValue(0);
 	labratory = 0;
-	factoryCrateSize = 25;
+	factoryCrateSize = 1000;//25;
 }
 
 DraftSchematicObjectTemplate::~DraftSchematicObjectTemplate() {
@@ -75,8 +75,8 @@ void DraftSchematicObjectTemplate::parseVariableData(const String& varName, LuaO
 		labratory = Lua::getIntParameter(state);
 	} else if (varName == "factoryCrateType") {
 		factoryCrateType = Lua::getStringParameter(state);
-	} else if (varName == "factoryCrateSize") {
-		factoryCrateSize = Lua::getIntParameter(state);
+	//} else if (varName == "factoryCrateSize") {
+		//factoryCrateSize = Lua::getIntParameter(state);
 	} else if (varName == "isMagic") {
 		isMagic = Lua::getBooleanParameter(state);
 	} else if (varName == "assemblySkill") {
