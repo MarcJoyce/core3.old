a7e520384e62f9bc36816d23be3ad5b8bd859d3f
Cyvster <41338761+Cyvster@users.noreply.github.com>
2024-03-28 04:34:01 -0500

    Update jedi_trials.lua
    jedi - set to padawan trials to award unlock on eligibility: jedi_trials.lua

--- MMOCoreORB/bin/scripts/screenplays/jedi/jedi_trials.lua
+++ MMOCoreORB/bin/scripts/screenplays/jedi/jedi_trials.lua
@@ -322,7 +322,8 @@ end
 
 function JediTrials:setStartedTrials(pPlayer)
 	if (self:isEligibleForPadawanTrials(pPlayer)) then
-		writeScreenPlayData(pPlayer, "PadawanTrials", "startedTrials", 1)
+		awardSkill(pPlayer, "force_title_jedi_rank_02")//writeScreenPlayData(pPlayer, "PadawanTrials", "startedTrials", 1)
+		writeScreenPlayData(pPlayer, "PadawanTrials", "completedTrials", 1)
 	elseif (self:isEligibleForKnightTrials(pPlayer)) then
 		writeScreenPlayData(pPlayer, "KnightTrials", "startedTrials", 1)
 	end
