a2b6d7904dfa2209bfe399c2bde836a707d9aa2f
Cyvster <41338761+Cyvster@users.noreply.github.com>
2024-04-07 18:05:41 -0500

    Update visibility_manager.lua
    jedi - set maxvisibility back to default, enabling bounty system: visibility_manager.lua

--- MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
@@ -1,4 +1,4 @@
-maxVisibility = 800      -- Maximum visibility
+maxVisibility = 8000      -- Maximum visibility
 termThreshold = 1500      -- Visibility Jedi will show on terminals
 falloffThreshold = 100    -- Visibility jedi will disappear off terminals
 totalDecayTimeInDays = 21 -- Total time before visibility decays from cap
